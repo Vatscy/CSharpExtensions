41a800f0945aedaa069a49318dc92b48c63e62a5
小林 正弥 <kobayashi_na@mti.co.jp>
2014-11-19 22:05:35 +0900

    object型の拡張メソッドを追加

---
+++ DoItYourself/Extensions/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DoItYourself.Extensions
+{
+	public static class ObjectExtensions
+	{
+		public static void ConsoleWriteLine(this object x)
+		{
+			Console.WriteLine(x);
+		}
+
+		public static void ConsoleWriteLine(this object x, string format)
+		{
+			Console.WriteLine(format, x);
+		}
+	}
+}
