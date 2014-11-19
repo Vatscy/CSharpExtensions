c669dc4f130fe62906edff17423e07b798c6dc3b
小林 正弥 <kobayashi_na@mti.co.jp>
2014-11-19 22:07:43 +0900

    シーケンスが重複しているかどうかを判断する処理を追加

--- DoItYourself.Tests/Extensions/LinqExtensionsTest.cs
+++ DoItYourself.Tests/Extensions/LinqExtensionsTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using DoItYourself.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DoItYourself.Tests.Extensions
@@ -8,10 +9,27 @@ namespace DoItYourself.Tests.Extensions
 	[TestClass]
 	public class LinqExtensionsTest
 	{
+		private class Test
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+		}
+
+		private static readonly List<Test> Source = Enumerable.Range(0, 10000).Select(s => new Test { Id = s, Name = s.ToString("00000") }).ToList();
+
+		[TestMethod]
+		public void AreUnique()
+		{
+			Utility.Time(() =>
+			{
+				Source.AreUnique(s => s.Id, s => s.Name);
+			}).ConsoleWriteLine("result: {0}ms");
+		}
+
 		[TestMethod]
 		public void Chunk()
 		{
-			var time = Utility.Time(() =>
+			Utility.Time(() =>
 			{
 				var total = 0;
 				var items = GetRange(1000).Chunk(9);
@@ -21,8 +39,7 @@ namespace DoItYourself.Tests.Extensions
 						total += i;
 					}
 				Console.WriteLine(total);
-			});
-			Console.WriteLine("result: {0}ms", time);
+			}).ConsoleWriteLine("result: {0}ms");
 		}
 
 		private IEnumerable<int> GetRange(int count)
